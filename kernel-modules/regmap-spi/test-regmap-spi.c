00cb0166fcd7c75b0a0d9750aadd873e1b279ace
Krzysztof Adamski <krzysztof.adamski@nokia.com>
2017-11-13 22:31:13 +0100

    kernel-modules: regmap-spi example

---
+++ kernel-modules/regmap-spi/test-regmap-spi.c
@@ -0,0 +1,76 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/of.h>
+#include <linux/spi/spi.h>
+#include <linux/regmap.h>
+
+const struct regmap_config test_spi_regmap_config = {
+	.reg_bits = 16,
+	.pad_bits = 8,
+	.val_bits = 32,
+
+	.cache_type = REGCACHE_NONE,
+	.read_flag_mask = 0x800000,
+};
+
+static int __init test_spi_init(void)
+{
+	pr_info("Hello World!\n");
+	return 0;
+}
+
+static void __exit test_spi_exit(void)
+{
+	pr_info("Bye World!\n");
+}
+
+static const struct of_device_id spi_test_match[] = {
+	{
+		.compatible = "k,test-regmap-spi",
+	},
+	{}
+};
+MODULE_DEVICE_TABLE(of, spi_test_match);
+
+static int test_spi_probe(struct spi_device *spi)
+{
+	struct regmap *regmap;
+	unsigned int val;
+
+	dev_info(&spi->dev, "probe %p!\n", spi);
+
+	regmap = devm_regmap_init_spi(spi, &test_spi_regmap_config);
+	if (IS_ERR(regmap)) {
+		dev_err(&spi->dev, "regmap_init failed with %ld\n", PTR_ERR(regmap));
+		return PTR_ERR(regmap);
+	}
+
+	regmap_write(regmap, 0x1234, 0x5678);
+	regmap_read(regmap, 0x1234, &val);
+	dev_info(&spi->dev, "val = %d\n", val);
+
+	return 0;
+}
+
+static int test_spi_remove(struct spi_device *spi)
+{
+	pr_info("remove %p!\n", spi);
+
+	return 0;
+}
+
+static const struct spi_driver test_spi_driver = {
+	.driver = {
+		.name = "test_spi",
+		.of_match_table = spi_test_match,
+	},
+	.probe = test_spi_probe,
+	.remove = test_spi_remove,
+};
+
+module_init(test_spi_init);
+module_exit(test_spi_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Krzysztof Adamski <k@japko.eu>");
